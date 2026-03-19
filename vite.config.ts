7125e95db61aa5445849902632f092993a877303
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2026-03-18 22:55:25 -0300

    chore: tentando diminuir o tamanho final dos executaveis

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+
 export default defineConfig({
   plugins: [react()],
   base: "./",
   build: {
     outDir: "dist/renderer",
     target: "baseline-widely-available",
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        dead_code: true,
+        passes: 3
+      },
+      mangle: true
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+        }
+      }
+    },
+    sourcemap: false,
+    chunkSizeWarningLimit: 500,
   },
   server: {
     port: 5173,
